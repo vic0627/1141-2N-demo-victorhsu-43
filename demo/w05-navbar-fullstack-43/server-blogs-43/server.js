67cd79a95232d60c0b63148d2592cc0cbe1c44d6
victor_xu <pure90719@gmail.com>
2025-10-19 16:33:09 +0800

    W05-P1: Create Navbar_43 using styled components, and show BlogNodePage_43

---
+++ demo/w05-navbar-fullstack-43/server-blogs-43/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import db from "./database.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/blog_43", async (req, res, next) => {
+  const results = await db.query("select * from blog_43");
+  console.log("results", JSON.stringify(results));
+  res.json(results);
+});
+
+app.use("/", (req, res, next) => {
+  res.send("VICTOR HSU, 213410243");
+});
+
+const port = process.env.PORT ?? 5000;
+
+app.listen(port, () => {
+  console.log(`server is running on port`, port);
+});
